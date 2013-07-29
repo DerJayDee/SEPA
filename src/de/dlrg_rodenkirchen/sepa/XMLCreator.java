c51bcaee136d44c730c29e7b5c7b07f8a8231048
Jan Degenhardt <jandegenhardt@web.de>
2013-07-29 13:29:44 +0200

    was getan

--- src/de/dlrg_rodenkirchen/sepa/XMLCreator.java
+++ src/de/dlrg_rodenkirchen/sepa/XMLCreator.java
@@ -2,7 +2,10 @@ package de.dlrg_rodenkirchen.sepa;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Stack;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Properties;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -23,10 +26,22 @@ import org.w3c.dom.Element;
 
 public class XMLCreator {
 
-	private Stack<Mitglied> mitglieder;
+	private ArrayList<Mitglied> mitglieder;
+
+	private static final String p_credName = "credName";
+	private static final String p_credID = "credId";
+	private static final String p_credIBAN = "credIBAN";
+	private static final String p_credBIC = "credBIC";
+	private static final String p_execDate = "execDate";
+
+	private Properties props;
+
+	public XMLCreator(Properties props) {
+		this.props = props;
+	}
 
 	public int readExcel(File xlsFile) throws BiffException, IOException {
-		mitglieder = new Stack<Mitglied>();
+		mitglieder = new ArrayList<Mitglied>();
 		File inputWorkbook = xlsFile;
 		Workbook w = Workbook.getWorkbook(inputWorkbook);
 		Sheet sheet = w.getSheet(0);
@@ -49,8 +64,7 @@ public class XMLCreator {
 			String betrag = cell.getContents();
 			Mitglied tmp = new Mitglied(nr, name, vorname, iban, bic, inhaber,
 					betrag);
-			mitglieder.push(tmp);
-			System.out.println(betrag);
+			mitglieder.add(tmp);
 		}
 		return mitglieder.size();
 	}
@@ -64,29 +78,29 @@ public class XMLCreator {
 		Document doc = docBuilder.newDocument();
 
 		// Document
-		Element root_Document = doc.createElement("Document");
-		doc.appendChild(root_Document);
-		// Namespaces
-		root_Document.setAttribute("xmlns",
-				"urn:iso:std:iso:20022:tech:xsd:pain.008.002.02");
-		root_Document.setAttribute("xmlns:xsi",
-				"http://www.w3.org/2001/XMLSchema-instance");
-		root_Document
-				.setAttribute("xsi:schemaLocation",
-						"urn:iso:std:iso:20022:tech:xsd:pain.008.002.02 pain.008.002.02.xsd");
+		Element document = createDocumentElem(doc);
+		doc.appendChild(document);
 
 		// CstmrDrctDbtInitn
 		Element root = doc.createElement("CstmrDrctDbtInitn");
-		root_Document.appendChild(root);
+		document.appendChild(root);
 
 		// GrpHdr
-		Element header = createGrpHdr(doc);
+		Element header = createGrpHdrElem(doc);
 		root.appendChild(header);
 
-		// salary elements
-		// Element salary = doc.createElement("salary");
-		// salary.appendChild(doc.createTextNode("100000"));
-		// staff.appendChild(salary);
+		// PmtInf
+		Element pmtInf = doc.createElement("PmtInf");
+		appendPmtInfHdr(pmtInf, doc);
+		appendCdtr(pmtInf, doc);
+
+		Element dbtr;
+		for(Mitglied m : mitglieder){
+			dbtr = createDbtr(m, doc);
+			pmtInf.appendChild(dbtr);
+		}
+
+		root.appendChild(pmtInf);
 
 		// XML schreiben
 		TransformerFactory transformerFactory = TransformerFactory
@@ -95,23 +109,187 @@ public class XMLCreator {
 		DOMSource source = new DOMSource(doc);
 		StreamResult result = new StreamResult(xmlFile);
 		transformer.transform(source, result);
-		System.out.println("File saved!");
+	}
+	
+	public void setProps(Properties props){
+		this.props = props;
 	}
 
-	private Element createGrpHdr(Document doc) {
+	private Element createDocumentElem(Document doc) {
+		Element root = doc.createElement("Document");
+		// Namespaces
+		root.setAttribute("xmlns",
+				"urn:iso:std:iso:20022:tech:xsd:pain.008.002.02");
+		root.setAttribute("xmlns:xsi",
+				"http://www.w3.org/2001/XMLSchema-instance");
+		root.setAttribute("xsi:schemaLocation",
+				"urn:iso:std:iso:20022:tech:xsd:pain.008.002.02 pain.008.002.02.xsd");
+
+		return root;
+	}
+
+	private Element createGrpHdrElem(Document doc) {
 		Element header = doc.createElement("GrpHdr");
-		Element tmp = doc.createElement("MsgId");
-		// TODO ID erzeugen
-		tmp.appendChild(doc.createTextNode("MID201305273145044"));
-		header.appendChild(tmp);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat();
 
-		tmp = doc.createElement("CreDtTm");
-		// TODO DateTime erzeugen
-		tmp.appendChild(doc.createTextNode("2013-05-27T08:44:10Z"));
-		header.appendChild(tmp);
+		// MsgID
+		Element msgId = doc.createElement("MsgId");
+		sdf.applyPattern("yyyyMMddHHmmssSS");
+		msgId.appendChild(doc
+				.createTextNode("MID" + sdf.format(new Date())));
+		header.appendChild(msgId);
 
-		// TODO
+		// CreDtTm
+		Element creDtTm = doc.createElement("CreDtTm");
+		sdf.applyPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+		creDtTm.appendChild(doc.createTextNode(sdf.format(new Date())));
+		header.appendChild(creDtTm);
+
+		// NbOfTxs
+		Element nbOfTxs = doc.createElement("NbOfTxs");
+		nbOfTxs.appendChild(doc.createTextNode("" + mitglieder.size()));
+		header.appendChild(nbOfTxs);
+
+		// CtrlSum
+		Element ctrlSum = doc.createElement("CtrlSum");
+		ctrlSum.appendChild(doc.createTextNode(getTotalSum()));
+		header.appendChild(ctrlSum);
+
+		// InitgPty
+		Element initgPty = doc.createElement("InitgPty");
+		Element nm = doc.createElement("Nm");
+		nm.appendChild(doc.createTextNode(props.getProperty(p_credName)));
+		initgPty.appendChild(nm);
+		header.appendChild(initgPty);
 
 		return header;
+	}
+
+	private String getTotalSum() {
+		double sum = 0.0;
+		for (Mitglied mitglied : mitglieder) {
+			sum += Double.parseDouble(mitglied.betrag.replace(',', '.'));
+		}
+		String sumStr = ""+sum;
+		if(sumStr.substring(sumStr.indexOf('.')).length() <= 3){
+			sumStr += "0";
+		}
+		return sumStr;
+	}
+
+	private void appendPmtInfHdr(Element pmtInf, Document doc) {
+		SimpleDateFormat sdf = new SimpleDateFormat();
+		
+		// PmtInfId
+		Element pmtInfId = doc.createElement("PmtInfId");
+		sdf.applyPattern("yyyyMMddHHmmssSS");
+		pmtInfId.appendChild(doc.createTextNode("PID" + sdf.format(new Date())));
+		pmtInf.appendChild(pmtInfId);
+
+		// PmtMtd
+		Element pmtMtd = doc.createElement("PmtMtd");
+		pmtMtd.appendChild(doc.createTextNode("DD"));
+		pmtInf.appendChild(pmtMtd);
+
+		// Btchbookg
+		Element btchBookg = doc.createElement("BtchBookg");
+		btchBookg.appendChild(doc.createTextNode("true"));
+		pmtInf.appendChild(btchBookg);
+
+		// NbOfTxs
+		Element nbOfTxs = doc.createElement("NbOfTxs");
+		nbOfTxs.appendChild(doc.createTextNode("" + mitglieder.size()));
+		pmtInf.appendChild(nbOfTxs);
+
+		// CtrlSum
+		Element ctrlSum = doc.createElement("CtrlSum");
+		ctrlSum.appendChild(doc.createTextNode(getTotalSum()));
+		pmtInf.appendChild(ctrlSum);
+
+		// PmtTpInf
+		Element pmtTpInf = doc.createElement("PmtTpInf");
+
+		Element svcLvl = doc.createElement("SvcLvl");
+		Element cd1 = doc.createElement("Cd");
+		cd1.appendChild(doc.createTextNode("SEPA"));
+		svcLvl.appendChild(cd1);
+		pmtTpInf.appendChild(svcLvl);
+
+		Element lclInstrm = doc.createElement("LclInstrm");
+		Element cd2 = doc.createElement("Cd");
+		cd2.appendChild(doc.createTextNode("CORE"));
+		lclInstrm.appendChild(cd2);
+		pmtTpInf.appendChild(lclInstrm);
+
+		Element seqTp = doc.createElement("SeqTp");
+		seqTp.appendChild(doc.createTextNode("FRST"));
+		pmtTpInf.appendChild(seqTp);
+
+		pmtInf.appendChild(pmtTpInf);
+
+		// ReqdColltnDt
+		Element reqdColltnDt = doc.createElement("ReqdColltnDt");
+		reqdColltnDt.appendChild(doc.createTextNode(props
+				.getProperty(p_execDate)));
+		pmtInf.appendChild(reqdColltnDt);
+	}
+
+	private void appendCdtr(Element pmtInf, Document doc) {
+		// Cdtr
+		Element cdtr = doc.createElement("Cdtr");
+		Element nm = doc.createElement("Nm");
+		nm.appendChild(doc.createTextNode(props.getProperty(p_credName)));
+		cdtr.appendChild(nm);
+		pmtInf.appendChild(cdtr);
+
+		// CdtrAcct
+		Element cdtrAcct = doc.createElement("CdtrAcct");
+		Element id1 = doc.createElement("Id");
+		Element iban = doc.createElement("IBAN");
+		iban.appendChild(doc.createTextNode(props.getProperty(p_credIBAN)));
+		id1.appendChild(iban);
+		cdtrAcct.appendChild(id1);
+		pmtInf.appendChild(cdtrAcct);
+
+		// CdtrAgt
+		Element cdtrAgt = doc.createElement("CdtrAgt");
+		Element finInstnId = doc.createElement("FinInstnId");
+		Element bic = doc.createElement("BIC");
+		bic.appendChild(doc.createTextNode(props.getProperty(p_credBIC)));
+		finInstnId.appendChild(bic);
+		cdtrAgt.appendChild(finInstnId);
+		pmtInf.appendChild(cdtrAgt);
+
+		// ChrgBr
+		Element chrgBr = doc.createElement("ChrgBr");
+		chrgBr.appendChild(doc.createTextNode("SLEV"));
+		pmtInf.appendChild(chrgBr);
+
+		// CdtrSchmeId
+		Element cdtrSchmeId = doc.createElement("CdtrSchmeId");
+		Element id2 = doc.createElement("Id");
+		Element prvtId = doc.createElement("PrvtId");
+		Element othr = doc.createElement("Othr");
+		Element id3 = doc.createElement("Id");
+		id3.appendChild(doc.createTextNode(props.getProperty(p_credID)));
+		Element schmeNm = doc.createElement("SchmeNm");
+		Element prtry = doc.createElement("Prtry");
+		prtry.appendChild(doc.createTextNode("SEPA"));
+		schmeNm.appendChild(prtry);
+		othr.appendChild(id3);
+		othr.appendChild(schmeNm);
+		prvtId.appendChild(othr);
+		id2.appendChild(prvtId);
+		cdtrSchmeId.appendChild(id2);
+		pmtInf.appendChild(cdtrSchmeId);
+	}
+
+	private Element createDbtr(Mitglied m, Document doc) {
+		Element dbtr = doc.createElement("Dbtr");
+
+		// FIXME fertig machen!
+
+		return dbtr;
 	}
 }
--- src/de/dlrg_rodenkirchen/sepa/Xsl2Xml.java
+++ src/de/dlrg_rodenkirchen/sepa/Xsl2Xml.java
@@ -1,16 +1,25 @@
 package de.dlrg_rodenkirchen.sepa;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.filechooser.FileFilter;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -21,25 +30,37 @@ public class Xsl2Xml extends JFrame {
 
 	private static final long serialVersionUID = 737038282745995221L;
 
+	private static final String propsName = "ressources/config.cfg";
+	private static final String p_credName = "credName";
+	private static final String p_credID = "credId";
+	private static final String p_credIBAN = "credIBAN";
+	private static final String p_credBIC = "credBIC";
+	private static final String p_execDate = "execDate";
+
+	private static final String tfl_credName = "Gläubiger:";
+	private static final String tfl_credID = "Gläubiger-ID:";
+	private static final String tfl_credIBAN = "Gläubiger-IBAN:";
+	private static final String tfl_credBIC = "Gläubiger-BIC:";
+	private static final String tfl_execDate = "Datum der Ausführung:";
+
 	private JButton open = new JButton("XLS-Datei öffnen...");
 	private JButton save = new JButton("XML-Datei speichern unter...");
+
+	private JTextField tf_credName;
+	private JTextField tf_credID;
+	private JTextField tf_credIBAN;
+	private JTextField tf_credBIC;
+	private JTextField tf_execDate;
+
+	private Properties props;
+
+	private GridBagConstraints gbc;
 
 	private XMLCreator xmlc;
 
 	public Xsl2Xml() {
-		this.setTitle("XLS SEPA Converter");
-		Container c = getContentPane();
-
-		// Buttons (Unten)
-		JPanel p1 = new JPanel();
-		// Open-Button
-		open.addActionListener(new OpenXLSListener());
-		p1.add(open);
-		// Save-Button
-		save.addActionListener(new SaveXMLListener());
-		save.setEnabled(false);
-		p1.add(save);
-		c.add(p1, BorderLayout.CENTER);
+		loadProps();
+		createGui();
 	}
 
 	class OpenXLSListener implements ActionListener {
@@ -51,18 +72,22 @@ public class Xsl2Xml extends JFrame {
 			int rVal = c.showOpenDialog(Xsl2Xml.this);
 			if (rVal == JFileChooser.APPROVE_OPTION) {
 				if (xmlc == null) {
-					xmlc = new XMLCreator();
+					xmlc = new XMLCreator(props);
 				}
 				try {
 					int count = xmlc.readExcel(c.getSelectedFile());
 					if (count > 0) {
 						save.setEnabled(true);
-						JOptionPane.showMessageDialog(c, "Anzahl eingelesene Einträge: " + count, "XLS eingelesen", JOptionPane.INFORMATION_MESSAGE);
+						JOptionPane.showMessageDialog(c,
+								"Anzahl eingelesene Einträge: " + count,
+								"XLS eingelesen",
+								JOptionPane.INFORMATION_MESSAGE);
 					} else {
-						JOptionPane.showMessageDialog(c, "Keine Einträge gefunden", "Fehler", JOptionPane.ERROR_MESSAGE);
+						JOptionPane.showMessageDialog(c,
+								"Keine Einträge gefunden", "Fehler",
+								JOptionPane.ERROR_MESSAGE);
 					}
 				} catch (BiffException | IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
 			}
@@ -74,30 +99,200 @@ public class Xsl2Xml extends JFrame {
 
 	class SaveXMLListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
+			saveProps();
 			JFileChooser c = new JFileChooser();
-			int rVal = c.showSaveDialog(Xsl2Xml.this);
-			if (rVal == JFileChooser.APPROVE_OPTION) {
-				try {
-					xmlc.writeXML(c.getSelectedFile());
-					JOptionPane.showMessageDialog(c, "XML-Datei erfolgreich geschrieben", "XML geschrieben", JOptionPane.INFORMATION_MESSAGE);
-				} catch (ParserConfigurationException | TransformerException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+			if (tf_credName.getText().equals("")
+					|| tf_credID.getText().equals("")
+					|| tf_credIBAN.getText().equals("")
+					|| tf_credBIC.getText().equals("")
+					|| tf_execDate.getText().equals("")) {
+				JOptionPane.showMessageDialog(c,
+						"Sie müssen alle Werte eingeben", "Fehlende Werte",
+						JOptionPane.ERROR_MESSAGE);
+			} else {
+				int rVal = c.showSaveDialog(Xsl2Xml.this);
+				if (rVal == JFileChooser.APPROVE_OPTION) {
+					xmlc.setProps(props);
+					try {
+						xmlc.writeXML(c.getSelectedFile());
+						JOptionPane.showMessageDialog(c,
+								"XML-Datei erfolgreich geschrieben",
+								"XML geschrieben",
+								JOptionPane.INFORMATION_MESSAGE);
+					} catch (ParserConfigurationException
+							| TransformerException e1) {
+						e1.printStackTrace();
+					}
+				}
+				if (rVal == JFileChooser.CANCEL_OPTION) {
+					// TODO Vielleicht mal was mit machen
 				}
 			}
-			if (rVal == JFileChooser.CANCEL_OPTION) {
-				// TODO Vielleicht mal was mit machen
-			}
+
 		}
 	}
 
+	class MainWindowListener implements WindowListener {
+
+		@Override
+		public void windowActivated(WindowEvent arg0) {
+			// Do nothing
+		}
+
+		@Override
+		public void windowClosed(WindowEvent arg0) {
+			// Do nothing
+		}
+
+		@Override
+		public void windowClosing(WindowEvent arg0) {
+			saveProps();
+			System.exit(0);
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent arg0) {
+			// Do nothing
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent arg0) {
+			// Do nothing
+		}
+
+		@Override
+		public void windowIconified(WindowEvent arg0) {
+			// Do nothing
+		}
+
+		@Override
+		public void windowOpened(WindowEvent arg0) {
+			// Do nothing
+		}
+
+	}
+
 	public static void main(String[] args) {
-		run(new Xsl2Xml(), 250, 110);
+		run(new Xsl2Xml(), 400, 200);
 	}
 
 	public static void run(JFrame frame, int width, int height) {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.setSize(width, height);
+		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
+	}
+
+	private void loadProps() {
+		if (props == null) {
+			props = new Properties();
+		}
+		try {
+			props.load(new FileInputStream(propsName));
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void saveProps() {
+		if (props == null) {
+			props = new Properties();
+		}
+		try {
+			// set all props
+			props.put(p_credName, tf_credName.getText());
+			props.put(p_credID, tf_credID.getText());
+			props.put(p_credIBAN, tf_credIBAN.getText());
+			props.put(p_credBIC, tf_credBIC.getText());
+			props.put(p_execDate, tf_execDate.getText());
+			// save properties to project root folder
+			props.store(new FileOutputStream(propsName), null);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	private void createGui() {
+		this.setTitle("XLS SEPA Converter");
+
+		this.addWindowListener(new MainWindowListener());
+		Container c = getContentPane();
+		JPanel mainP = new JPanel();
+		mainP.setLayout(new GridBagLayout());
+		gbc = new GridBagConstraints();
+
+		// Text Pannel
+		JPanel p1 = new JPanel();
+		p1.setLayout(new GridBagLayout());
+
+		// credName
+		addLabel(tfl_credName, p1, 0, 0);
+		addTextField(tf_credName = new JTextField(
+				props.getProperty(p_credName), 20), p1, 1, 0);
+
+		// credID
+		addLabel(tfl_credID, p1, 0, 1);
+		addTextField(
+				tf_credID = new JTextField(props.getProperty(p_credID), 20),
+				p1, 1, 1);
+
+		// credIBAN
+		addLabel(tfl_credIBAN, p1, 0, 2);
+		addTextField(tf_credIBAN = new JTextField(
+				props.getProperty(p_credIBAN), 20), p1, 1, 2);
+
+		// credBIC
+		addLabel(tfl_credBIC, p1, 0, 3);
+		addTextField(tf_credBIC = new JTextField(props.getProperty(p_credBIC),
+				20), p1, 1, 3);
+
+		// execDate
+		addLabel(tfl_execDate, p1, 0, 4);
+		addTextField(tf_execDate = new JTextField(
+				props.getProperty(p_execDate), 20), p1, 1, 4);
+
+		// ButtonPannel
+		// Buttons
+		JPanel p2 = new JPanel();
+		p2.setLayout(new GridBagLayout());
+		// Open-Button
+		open.addActionListener(new OpenXLSListener());
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		p2.add(open, gbc);
+		// Save-Button
+		save.addActionListener(new SaveXMLListener());
+		save.setEnabled(false);
+		gbc.gridy = 1;
+		p2.add(save, gbc);
+
+		// Assemble
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		mainP.add(p1, gbc);
+
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		mainP.add(p2, gbc);
+
+		c.add(mainP);
+	}
+
+	private void addLabel(String name, Container c, int gridx, int gridy) {
+		JLabel label = new JLabel(name, SwingConstants.LEFT);
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		c.add(label, gbc);
+	}
+
+	private void addTextField(JTextField tf, Container c, int gridx, int gridy) {
+		gbc.gridx = gridx;
+		gbc.gridy = gridy;
+		gbc.weightx = 0.5;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+		c.add(tf, gbc);
 	}
 }
