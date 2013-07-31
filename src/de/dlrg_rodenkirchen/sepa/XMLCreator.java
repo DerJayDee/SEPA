9e124ab5076620cab96c4394549d52875a74675e
Jan Degenhardt <jandegenhardt@web.de>
2013-07-31 12:58:48 +0200

    cleanup

--- src/de/dlrg_rodenkirchen/sepa/XMLCreator.java
+++ src/de/dlrg_rodenkirchen/sepa/XMLCreator.java
@@ -3,7 +3,6 @@ package de.dlrg_rodenkirchen.sepa;
 import java.io.File;
 import java.io.IOException;
 import java.text.NumberFormat;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
